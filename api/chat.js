10b89fea0a81059a1f8c0fc4bfd000f7e87b82c9
StackItHigh <stewartjamespayne@gmail.com>
2025-06-24 23:47:31 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -155,7 +155,7 @@ ${tokenData.chain ? `⛓️ **Chain:** ${tokenData.chain.toUpperCase()}` : ''}
 ${signalData.signal} **Signal:** ${signalData.signal.split(' ').slice(1).join(' ')} - *${signalData.reasoning}*`;
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
